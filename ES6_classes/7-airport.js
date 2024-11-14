beac4ad6fd0095a345d0eea4ddd6546daf7e2a58
Kevrentas <kevrentas3@gmail.com>
2024-11-14 17:18:13 -0400

    ya

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,16 @@
+export default class Airport {
+    /**
+       *
+       * @param {String} name
+       * @param {String} code
+       */
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;
+    }
+  
+    toString() {
+      return `[object ${this._code}]`;
+    }
+  }
+  
