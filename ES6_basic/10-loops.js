8a3969cfae0f6a019fd4eb504db60c1c46784447
Kevrentas <kevrentas3@gmail.com>
2024-11-13 23:01:41 -0400

    Revert "ES6"
    This reverts commit 9aa5e7cb18c9bdb8c17befe1d4e8c2d81f213574.

--- ES6_basic/10-loops.js
+++
@@ -1,8 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-    const newarray = [];
-    for (const value of array) {
-      newarray.push(appendString + value);
-    }
-    return newarray;
-  }
-  
